0bdefe7317be398dd709b43f3c0d9b632e24894e
Simon Anders <anders@ebi.ac.uk>
2009-03-26 18:42:22 +0000

    removed function 'HilbertDisplayOld'
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/HilbertVisGUI@38240 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/HilbertCurveDisplay.R
+++ R/HilbertCurveDisplay.R
@@ -9,51 +9,6 @@ simpleLinPlot <- function( data, info ) {
       shrinkLength = min( 2*hw + 1, 4000 ) ) }    
 
 
-hilbertDisplayOld <- function( 
-      ..., 
-      palette = hilbertDefaultPalette( 1 + min( 1000, max(..., na.rm=TRUE) ) ),
-      paletteSteps = 1:(ncol(palette)-1),
-      naColor = col2rgb( "gray" ), 
-      plotFunc = simpleLinPlot, 
-      names=NULL,
-      sameScale=FALSE,
-      pow2=FALSE,
-      portrait=TRUE,
-      fullLengths = NULL ) {
-      
-   numargs <- length( dotsapply( function(x) NULL, ... ) )
-   if( numargs == 0)
-      stop( "Supply at least one data vector." )
-   stopifnot( is.function( plotFunc ) )
-   stopifnot( nrow( palette ) == 3 )
-   stopifnot( length( naColor ) == 3 )
-   if( is.null(names) )
-      names <- sapply( substitute(list(...)), deparse )[-1]
-   stopifnot( is.character( names ) )
-   if( sameScale ) {
-      if( !is.null(fullLengths) )
-         warning( "Argument 'fullLengths' overriden by argument 'sameScale'." )
-      maxlength <- max( unlist( dotsapply( length, ... ) ) )
-      fullLengths <- rep( maxlength, numargs )
-   }
-   if( pow2 ) {
-      if( is.null(fullLengths) )
-         fullLengths <- unlist( dotsapply( length, ... ) )
-      fullLengths <- sapply( fullLengths, function(a)
-         2**ceiling( log( a, 2 ) ) )
-   }
-   if( ! is.null( fullLengths ) )
-      fullLengths <- as.integer( fullLengths )
-   if( min(...) < 0 )
-      warning( "Data contains negative values. The current implementation may display these incorrectly." )
-      
-   .External( `R_display_hilbert_old`, plotFunc, 
-      names, as.integer( palette ), as.integer( naColor ), 
-      as.numeric( paletteSteps ), fullLengths, portrait, ... )   
-      
-   invisible( )
-}
-
 hilbertDisplayThreeChannel <- function( 
       dataRed, dataGreen, dataBlue, 
       naColor = col2rgb( "gray" ),
